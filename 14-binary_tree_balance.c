39359eb61e607d93256265005073d97e74fdf7cf
meenakshee23 <meenaksheegopaul@gmail.com>
2025-09-07 19:47:28 +0400

    new file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,25 +3,25 @@
 /**
  * binary_tree_height - finds the height
  * @tree: pointer
- * Return: 0 if tree is NULL
+ * Return: -1 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
+	size_t left = -1, right = -1;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	if (tree->left != NULL)
-		left = 1 + binary_tree_height(tree->left);
+		left = binary_tree_height(tree->left);
 
 	if (tree->right != NULL)
-		right = 1 + binary_tree_height(tree->right);
+		right = binary_tree_height(tree->right);
 
 	if (left > right)
-		return (left);
+		return (1 + left);
 	else
-		return (right);
+		return (1 + right);
 }
 
 /**
