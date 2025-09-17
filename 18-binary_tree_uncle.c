5d3f17e7d3f74184a1b3f7c9e33b16087509cd50
meenakshee23 <meenaksheegopaul@gmail.com>
2025-09-17 23:30:09 +0400

    new file

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,3 +7,12 @@
  * Return: a pointer to the uncle node or NULL if no uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}
